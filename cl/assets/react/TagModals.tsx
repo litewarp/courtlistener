b80edd726b4229919968b71065f433b06d944387
Michael Lissner <mlissner@michaeljaylissner.com>
2020-09-23 15:43:08 -0700

    refactor(tags): Remove extra <> thing

--- cl/assets/react/TagModals.tsx
+++ cl/assets/react/TagModals.tsx
@@ -87,114 +87,109 @@ const TagEditModal: React.FC<ModalProps> = ({ tag, setTag, show, setShow, userId
   });
 
   return (
-    <>
-      <Modal show={show} onHide={handleClose} animation={false}>
-        <Modal.Header closeButton>
-          <Modal.Title componentClass="h2">Edit Tag</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          {!tag ? (
-            <>
-              <i className="fa fa-spinner fa-pulse gray" />
-              &nbsp;Loading...
-            </>
-          ) : (
-            <>
-              <form className="form-horizontal" onSubmit={handleSubmit(onSubmit)}>
-                <input type="hidden" value={tag.id} name="id" ref={register} />
-                <div className={!errors.name ? 'form-group' : 'form-group has-error'}>
-                  <label htmlFor="name" className="col-sm-2 control-label">
-                    Tag Name
-                  </label>
-                  <div className="col-sm-10">
-                    <input
-                      type="text"
-                      className="form-control"
-                      name="name"
-                      placeholder="A name for your tag..."
-                      defaultValue={tag ? tag.name : ''}
-                      ref={register({
-                        required: true,
-                        pattern: /^[a-z0-9-]*$/,
-                        validate: validateTagDoesNotExist,
-                      })}
-                    />
-                    <p className="gray">
-                      <i className="fa fa-info-circle" /> Note that changing the tag name changes its link, and your
-                      bookmarks or browser history may fail.
+    <Modal show={show} onHide={handleClose} animation={false}>
+      <Modal.Header closeButton>
+        <Modal.Title componentClass="h2">Edit Tag</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        {!tag ? (
+          <>
+            <i className="fa fa-spinner fa-pulse gray" />
+            &nbsp;Loading...
+          </>
+        ) : (
+          <>
+            <form className="form-horizontal" onSubmit={handleSubmit(onSubmit)}>
+              <input type="hidden" value={tag.id} name="id" ref={register} />
+              <div className={!errors.name ? 'form-group' : 'form-group has-error'}>
+                <label htmlFor="name" className="col-sm-2 control-label">
+                  Tag Name
+                </label>
+                <div className="col-sm-10">
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="name"
+                    placeholder="A name for your tag..."
+                    defaultValue={tag ? tag.name : ''}
+                    ref={register({
+                      required: true,
+                      pattern: /^[a-z0-9-]*$/,
+                      validate: validateTagDoesNotExist,
+                    })}
+                  />
+                  <p className="gray">
+                    <i className="fa fa-info-circle" /> Note that changing the tag name changes its link, and your
+                    bookmarks or browser history may fail.
+                  </p>
+                  {errors.name?.type === 'required' && <p className="has-error help-block">This field is required.</p>}
+                  {errors.name?.type === 'pattern' && (
+                    <p className="has-error help-block">
+                      Only lowercase letters, numbers, and &apos;-&apos; are allowed.
                     </p>
-                    {errors.name?.type === 'required' && (
-                      <p className="has-error help-block">This field is required.</p>
-                    )}
-                    {errors.name?.type === 'pattern' && (
-                      <p className="has-error help-block">
-                        Only lowercase letters, numbers, and &apos;-&apos; are allowed.
-                      </p>
-                    )}
-                    {errors.name?.type === 'validate' && (
-                      <p className="has-error help-block">You already have a tag with that name.</p>
-                    )}
-                  </div>
+                  )}
+                  {errors.name?.type === 'validate' && (
+                    <p className="has-error help-block">You already have a tag with that name.</p>
+                  )}
                 </div>
-                <div className="form-group">
-                  <label htmlFor="title" className="col-sm-2 control-label">
-                    Title
-                  </label>
-                  <div className="col-sm-10">
-                    <input
-                      type="text"
-                      className="form-control"
-                      name="title"
-                      placeholder="A brief, one-line summary of your tag..."
-                      defaultValue={tag ? tag.title : ''}
-                      ref={register}
-                    />
-                  </div>
+              </div>
+              <div className="form-group">
+                <label htmlFor="title" className="col-sm-2 control-label">
+                  Title
+                </label>
+                <div className="col-sm-10">
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="title"
+                    placeholder="A brief, one-line summary of your tag..."
+                    defaultValue={tag ? tag.title : ''}
+                    ref={register}
+                  />
                 </div>
-                <div className="form-group">
-                  <label htmlFor="description" className="col-sm-2 control-label">
-                    Description
-                  </label>
-                  <div className="col-sm-10">
-                    <p className="gray">
-                      Provide any additional comments you have about this tag, describing the kinds of dockets it
-                      contains or why you created it.
-                    </p>
-                    <textarea
-                      className="form-control"
-                      name="description"
-                      placeholder="A long description of your tag..."
-                      rows={6}
-                      defaultValue={tag ? tag.description : ''}
-                      ref={register}
-                    />
-                    <p className="text-right">
-                      <a href="/help/markdown/">Markdown Supported</a>
-                    </p>
-                  </div>
+              </div>
+              <div className="form-group">
+                <label htmlFor="description" className="col-sm-2 control-label">
+                  Description
+                </label>
+                <div className="col-sm-10">
+                  <p className="gray">
+                    Provide any additional comments you have about this tag, describing the kinds of dockets it contains
+                    or why you created it.
+                  </p>
+                  <textarea
+                    className="form-control"
+                    name="description"
+                    placeholder="A long description of your tag..."
+                    rows={6}
+                    defaultValue={tag ? tag.description : ''}
+                    ref={register}
+                  />
+                  <p className="text-right">
+                    <a href="/help/markdown/">Markdown Supported</a>
+                  </p>
                 </div>
+              </div>
 
-                <div className="form-group">
-                  <div className="col-sm-offset-2 col-sm-10">
-                    <div className="checkbox">
-                      <label>
-                        <input type="checkbox" ref={register} name="published" defaultChecked={tag.published} />{' '}
-                        Published
-                      </label>
-                    </div>
+              <div className="form-group">
+                <div className="col-sm-offset-2 col-sm-10">
+                  <div className="checkbox">
+                    <label>
+                      <input type="checkbox" ref={register} name="published" defaultChecked={tag.published} /> Published
+                    </label>
                   </div>
                 </div>
-              </form>
-            </>
-          )}
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={handleSubmit(onSubmit)} bsStyle="primary" bsSize="large">
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
+              </div>
+            </form>
+          </>
+        )}
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={handleSubmit(onSubmit)} bsStyle="primary" bsSize="large">
+          Save Changes
+        </Button>
+      </Modal.Footer>
+    </Modal>
   );
 };
 
