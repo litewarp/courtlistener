61ebd7a2ee6ed194b8405583760a3cc498b3e50d
Michael Lissner <mlissner@michaeljaylissner.com>
2020-09-24 14:14:17 -0700

    fix(markdown): Simply the markdown we support

--- cl/settings/10-public.py
+++ cl/settings/10-public.py
@@ -411,13 +411,6 @@ MARKDOWN_DEUX_STYLES = {
             "tables": None,
         },
         "safe_mode": "escape",
-        "link_patterns": [
-            (
-                re.compile(r"network\s+#?(\d+)\b", re.I),
-                r"/visualizations/scotus-mapper/\1/md/",
-            ),
-            (re.compile(r"opinion\s+#?(\d+)\b", re.I), r"/opinion/\1/md/"),
-        ],
     },
 }
 
